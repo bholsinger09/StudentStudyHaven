cd2f0111b278136d401b12fa9840253afe97685f
ben <bholsinger@gmail.com>
2026-09-12 13:33:05 -0700

    Add App.framework to iOS project linkage and simplify iOSAppWrapper

---
+++ iOSAppWrapper 2.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import App
+
+@main
+struct iOSAppWrapper: App {
+    var body: some Scene {
+        WindowGroup {
+            StudentStudyHavenApp()
+        }
+    }
+}
---
+++ iOSAppWrapper.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import App
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StudentStudyHavenApp()
+        }
+    }
+}
